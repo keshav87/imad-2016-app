02c6d5bc04bfb8600aeb95aae0a933d61c0534f3
keshav87 <keshavg214@gmail.com>
2016-09-23 10:42:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change text
 var element= document.getElementById('main-text');
 element.innerHTML='my nake is keshav gupta';
+//move the image
+var img=document.getElementById('img1');
+img1.onclick=function(){
+    img1.style.marginLeft='100px';
+};
