cf5a9c97e58eb91e57d519b77d7ca31c9f96e801
keshav87 <keshavg214@gmail.com>
2016-09-23 10:48:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element= document.getElementById('main-text');
 element.innerHTML='my nake is keshav gupta';
 //move the image
 var img=document.getElementById('img1');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+5;
+    img1.style.marginLeft=marginLeft+'px';
+}
 img1.onclick=function(){
-    img1.style.marginLeft='100px';
+    var interval=setIntervel(moveRight,500);
+    //img1.style.marginLeft='100px';
 };
