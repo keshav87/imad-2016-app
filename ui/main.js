1d59abb15b507b457be1965a7971ec1f9a41cf81
keshav87 <keshavg214@gmail.com>
2016-09-24 12:47:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,19 @@
-// console.log('Loaded!');
-// //change text
-// var element= document.getElementById('main-text');
-// element.innerHTML='my nake is keshav gupta';
-// //move the image
-// var img=document.getElementById('img1');
-// var marginLeft=0;
-// function moveRight()
-// {
-//     marginLeft=marginLeft+5;
-//     img1.style.marginLeft=marginLeft+'px';
-// }
-// img1.onclick=function(){
-//     var interval=setInterval(moveRight,500);
-//     //img1.style.marginLeft='100px';
-// };
+console.log('Loaded!');
+//change text
+var element= document.getElementById('main-text');
+element.innerHTML='my nake is keshav gupta';
+//move the image
+var img=document.getElementById('img1');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+5;
+    img1.style.marginLeft=marginLeft+'px';
+}
+img1.onclick=function(){
+    var interval=setInterval(moveRight,500);
+    //img1.style.marginLeft='100px';
+};
 
 var button = document.getElementById('counter');
 var counter=0;
